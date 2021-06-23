32f78afe9d48c126f613a4df658742956fbc2d5e
Chen Zu Sheep <mxh0801@gmail.com>
2021-06-24 02:01:38 +0800

    v0.3
     - 1.移除清屏控制符（效果不佳）.
    + 2.新增：踩完所有无雷区域则赢.
    + 3.增加对输入无效行列数和地雷数的异常处理.
     * 4.优化布局.
     * 5.优化部分代码逻辑.

--- Boom/src/test/boomshakalaka/Boom.java
+++ Boom/src/test/boomshakalaka/Boom.java
@@ -5,81 +5,91 @@ import java.util.Random;
 public class Boom {
 
     private int[][] gameBoard;
-
-    private final int rows, columns;
+    private int treadCount;
+    private final int rows, columns, voidCount;
 
     // 构造方法
-    Boom (int rows, int columns, int n) {
-
-        this.rows = rows;
-        this.columns = columns;
+    Boom(int rows, int columns, int boomCount) {
 
         Random rd = new Random();
 
+        this.rows = rows;
+        this.columns = columns;
         // 随机雷数
-        if(n==-1) while (n<=0) n = rd.nextInt((int)(rows * columns * 0.75));
+        if(boomCount==-1) while (boomCount<=0) boomCount = rd.nextInt((int)(rows * columns * 0.75));
+        this.voidCount = rows*columns-boomCount;
 
         // 指定雷数
-        if (n>0 && n<rows*columns*0.75) {
+        if(boomCount>0 && boomCount<rows*columns*0.75) {
             gameBoard = new int[rows][columns];
-            for (int i=0; i<n; i++) {
+            for(int i=0; i<boomCount; i++) {
                 int x = rd.nextInt(rows), y = rd.nextInt(columns);
                 if (gameBoard[x][y] == -1) i--;
                 else gameBoard[x][y] = -1;
             }
-        }
-        else {
+        } else {
             System.out.println("布雷数超出合法范围");
             System.exit(-1);
         }
     }
 
     // 输出列号
-    private void printColumnNum(){
-		System.out.print("\n"+"\t");
-        for(int i=1; i<=columns; i++){
+    private void printColumnNum() {
+		System.out.print("\t\t");
+        for(int i=1; i<=columns; i++) {
             System.out.print(i+"\t");
         }
-        System.out.println();
+        System.out.print("\n\n");
     }
 
     // 输出
-    public void printBoard(boolean showBooms){
+    public void printBoard(int showBooms){
 
-        // 清屏(仅Windows CMD环境下有效)
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        /*
+         清屏(仅Windows CMD环境下有效)
+         System.out.print("\033[H\033[2J");
+         System.out.flush();
+        */
 
         printColumnNum();
-        for(int i=0; i<rows; i++){
-            System.out.print("\n"+String.valueOf(i+1)+"\t");  // 输出行号
-            for(int j=0; j<columns; j++){
-                if(gameBoard[i][j] == -1 && showBooms) {
+        for(int i=0; i<rows; i++) {
+            System.out.print("\t"+ (i+1) +"\t");  // 输出行号
+            for(int j=0; j<columns; j++) {
+                if(gameBoard[i][j] == -1 && showBooms==1) {
                 	System.out.print("@"+"\t"); // 显示炸弹
-                }
-                else if(gameBoard[i][j] == 1) {
+                } else if (gameBoard[i][j] == 1) {
                 	System.out.print("-"+"\t");  // 已踩点
-                }
-                else {
+                } else {
                 	System.out.print("●"+"\t");  // 未踩点
                 }
             }
-            System.out.print(String.valueOf(i+1)+"\t\n");
+            System.out.print((i+1) +"\n\n");
         }
         printColumnNum();
+        // System.out.println("\t"+" ● 未踩 "+"\t\t"+" - 已踩 "+"\t\t"+" @ 地雷 "+"\t");
     }
 
     // 踩点
-    public void tread(int x, int y){
+    public int tread(int x, int y) {
         //中雷
-        if(gameBoard[x-1][y-1] == -1){
-            printBoard(true);
-            System.out.println("\n"+"你死了！"+"\n");
-            System.exit(0);
-        }
-        else{
+        if(gameBoard[x-1][y-1] == -1) {
+            printBoard(1);
+            System.out.println("你死了！");
+            return -1;
+
+        } else {
             gameBoard[x-1][y-1] = 1;
-            printBoard(false);
+            treadCount++;
+            // 踩完所有无雷位置则赢
+            if(treadCount==voidCount) {
+                printBoard(1);
+                System.out.println("你赢了！");
+                return 1;
+
+            } else {
+                printBoard(0);
+                return 0;
+            }
         }
     }
 }
--- Boom/src/test/boomshakalaka/Main.java
+++ Boom/src/test/boomshakalaka/Main.java
@@ -4,32 +4,43 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int r, c, n;
 
         @SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 
-        System.out.print("请输入行数，列数(以逗号分隔):");
-        String[] params = sc.next().split(",");
-        r = Integer.parseInt(params[0]);
-        c = Integer.parseInt(params[1]);
+        while(true) {
+            try {
+                System.out.print("请输入行数，列数(以逗号分隔):");
+                String[] params = sc.next().split(",");
+                r = Integer.parseInt(params[0]);
+                c = Integer.parseInt(params[1]);
 
-        System.out.print("请输入地雷个数(输入-1为随机):");
-        n = sc.nextInt();
+                System.out.print("请输入地雷个数(输入-1为随机):");
+                n = sc.nextInt();
+                System.out.println();
+
+                break;
+
+            } catch(Exception e) {
+                System.out.println("输入无效，请检查输入！\n");
+            }
+        }
 
         Boom tanmou = new Boom(r, c, n);
 
-        tanmou.printBoard(false);
+        tanmou.printBoard(0);
 
-        while (true){
-            System.out.print("\n"+"请输入扫雷位置，以(行,列)表示:");
-            params = sc.next().split(",");
+        int statusFlag = 0;
+        while(statusFlag == 0) {
+            System.out.print("请输入扫雷位置，以(行,列)表示:");
+            String[] params = sc.next().split(",");
+            System.out.println();
             try {
-                tanmou.tread(Integer.parseInt(params[0]), Integer.parseInt(params[1]));
-            }
-            catch (Exception e) {
+                statusFlag = tanmou.tread(Integer.parseInt(params[0]), Integer.parseInt(params[1]));
+            } catch(Exception e) {
                 System.out.println("所输入位置无效，请重新输入！");
             }
         }
